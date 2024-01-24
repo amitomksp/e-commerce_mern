6c79596c42018217f0fe93393f48284ad01cf1f4
amit om kashyap <amit.ece.21@nitap.ac.in>
2024-01-24 23:50:33 +0530

    Day4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <LoginPage></LoginPage>
     </div>
   );
 }
